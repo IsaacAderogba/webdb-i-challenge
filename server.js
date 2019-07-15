7504f78b0ae536963a6022b33fc75960e14f6e35
Isaac Aderogba <isaacaderogba1@gmail.com>
2019-07-15 12:47:14 +0100

    Add error handling middleware

--- server.js
+++ server.js
@@ -1,9 +1,19 @@
-const express = require('express');
+const express = require("express"); // import express
+// const accountsRouter = require('./routes/accountsRouter');
 
-const db = require('./data/dbConfig.js');
+const db = require("./data/dbConfig.js"); // pull data
+const server = express(); // use express
 
-const server = express();
+server.use(express.json()); // use json
 
-server.use(express.json());
+server.get("/", (req, res, next) => {
+  next(new Error('Error'));
+  res.send(`<h2>WebDB I Challenge</h2>`);
+});
 
-module.exports = server;
+// Error handling middleware
+server.use((err, req, res, next) => {
+  res.status(500).json({ message: err.message });
+});
+
+module.exports = server; // export server
